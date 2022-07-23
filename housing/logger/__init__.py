57f6f29843ec3c256aaf3a6390730c256bf4222b
Praveenmurali986 <praveenmurali986@gmail.com>
2022-07-23 20:52:39 +0530

    '---'

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,21 +1,31 @@
-from distutils.log import INFO
+
 import logging
 from datetime import datetime
 import os
 
 import pandas as pd
+from housing.constant import get_current_time_stamp
 
 
 
-LOG_Dir='housing_log'
 
-CURRENT_TIME_STAMP=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+LOG_DIR='logs'
 
-os.makedirs(LOG_Dir,exist_ok=True)
+def get_log_file_name():
+    return f'log_{get_current_time_stamp()}.log'
 
-LOG_FILE_PATH=os.path.join(LOG_Dir,LOG_FILE_NAME)
+LOG_FILE_NAME=get_log_file_name()
+
+os.makedirs(LOG_DIR,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+
+
+
+
+
 
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode='w',
