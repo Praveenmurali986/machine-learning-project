303f50909a9d75a8467ba96cd8a5e2d9a782423f
Praveenmurali986 <praveenmurali986@gmail.com>
2022-07-24 19:56:45 +0530

    'update'

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-from lib2to3.pgen2.token import NAME
-from unicodedata import name
-from pytz import VERSION
-from setuptools import setup
 from setuptools import setup,find_packages
-from tables import Description
 from typing import List
-import numpy as np
 
 
 
 
-
-NAME='housing_predictor'
-VERSION='0.0.3'
-AUTHOR='praveen'
-DESCRIPTION='First ml project'
-PACKAGES=['housing']
+PROJECT_NAME="housing_predictor"
+VERSION="0.0.3"
+AUTHOR="praveen_m"
+DESCRIPTION= "ml project"
 REQUIREMENT_FILE_NAME='requirements.txt'
+HYPHEN_E_DOT="-e ."
 
 
 def get_requirements_list()->List[str]:
@@ -27,17 +20,19 @@ def get_requirements_list()->List[str]:
 
     '''
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove('-e .')
-
+       requirement_list=requirement_file.readlines()
+       requirement_list=[requirement_name.replace("\n","") for requirement_name in requirement_list]
+       if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
 setup(
-name=NAME,
+name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
 packages=find_packages(),
 install_requires=get_requirements_list()
-
 )
 
 
